38a1802264b67739c830177a0c8e553401c27852
Declan <porter.declan@gmail.com>
2020-08-24 10:21:33 +0200

    Added EditLecturerInfo, Added EditModuleDetails removed ModuleDetails.

--- EditLecturerInformation.aspx.cs
+++ EditLecturerInformation.aspx.cs
@@ -7,7 +7,7 @@ using System.Web.UI.WebControls;
 
 namespace WebApplication3
 {
-    public partial class ModuleDetails : System.Web.UI.Page
+    public partial class EditLecturerInformation : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
